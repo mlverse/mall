6c8d0d138539ea92f74a621545c46428b869bc29
Edgar Ruiz <edgararuiz@gmail.com>
2024-08-31 14:10:15 -0500

    renames l_generate to llm_backend_generate, adds script

---
+++ R/llm-backend-generate.R
@@ -0,0 +1,29 @@
+#' @export
+llm_backend_generate <- function(backend, x, base_prompt) {
+  UseMethod("llm_backend_generate")
+}
+
+#' @export
+llm_backend_generate.list <- function(backend, x, base_prompt) {
+  try_connection <- ollamar::test_connection()
+  
+  if(try_connection$status_code == 200) {
+    .env_llm$defaults$model <- "llama3.1" 
+    class(.env_llm$defaults) <- "ollama"
+    return(llm_backend_generate(.env_llm$defaults, x, base_prompt))
+  }
+  invisible()
+}
+
+#' @export
+llm_backend_generate.ollama <- function(backend, x, base_prompt) {
+  map_chr(x, ~ {
+    ollamar::generate(
+      model = backend$model,
+      prompt = glue("{base_prompt} {.x}"),
+      output = "text"
+    )
+  },
+  .progress = TRUE
+  )
+}
