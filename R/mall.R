ccc5f87b59247269294153c7ed5dbf735b8c399c
Edgar Ruiz <edgararuiz@gmail.com>
2024-09-05 12:44:51 -0500

    Adds defaults get and set functions

--- R/mall.R
+++ R/mall.R
@@ -7,3 +7,19 @@
 
 .env_llm <- new.env()
 .env_llm$defaults <- list()
+defaults_get <- function() {
+  .env_llm$defaults
+}
+
+defaults_set <- function(..., .quiet = TRUE) {
+  new_args <- list(...)
+  for (i in seq_along(new_args)) {
+    nm <- names(new_args[i])
+    .env_llm$defaults[[nm]] <- new_args[[i]]
+  }
+  if (!.quiet) {
+    return(defaults_get())
+  } else {
+    invisible()
+  }
+}
