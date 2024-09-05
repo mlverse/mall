88351beaa903096d4002b19c1a6b159390dfe5bb
Edgar Ruiz <edgararuiz@gmail.com>
2024-09-05 13:31:04 -0500

    Warning only displays if there are exceptions

--- R/llm-vec-generate.R
+++ R/llm-vec-generate.R
@@ -5,9 +5,11 @@ llm_vec_generate <- function(x, prompt, valid_resps = NULL) {
   if (!is.null(valid_resps)) {
     errors <- !resp %in% valid_resps
     resp[errors] <- NA
-    cli_alert_warning(
-      "There were {sum(errors)} predictions with invalid output, they were coerced to NA"
-    )
+    if(any(errors)) {
+      cli_alert_warning(
+        "There were {sum(errors)} predictions with invalid output, they were coerced to NA"
+      )  
+    }
   }
   resp
 }
