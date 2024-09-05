42104bb1bbc3d45eb0568b7adcb282bf8619093e
Edgar Ruiz <edgararuiz@gmail.com>
2024-09-05 10:18:58 -0500

    Outputs NA for invalid output, outputs warning

--- R/llm-vec-generate.R
+++ R/llm-vec-generate.R
@@ -2,7 +2,11 @@
 llm_vec_generate <- function(x, prompt, valid_resps = NULL) {
   resp <- llm_backend_generate(.env_llm$defaults, x, prompt)
   if (!is.null(valid_resps)) {
-    resp[!resp %in% valid_resps] <- "#err"
+    errors <- !resp %in% valid_resps
+    resp[errors] <- NA
+    cli_alert_warning(
+      "There were {sum(errors)} predictions with invalid output, they were coerced to NA"
+    )
   }
   resp
 }
