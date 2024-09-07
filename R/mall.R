fe33378bf47d532dfe38a94317d8c76532168e8b
Edgar Ruiz <edgararuiz@gmail.com>
2024-09-07 13:03:47 -0500

    Ver bump, expands DESCRIPTION, fixes imports

--- R/mall.R
+++ R/mall.R
@@ -1,8 +1,9 @@
-#' @importFrom purrr map_chr
-#' @importFrom dplyr mutate tibble bind_cols
 #' @importFrom ollamar generate test_connection list_models
-#' @import glue
+#' @importFrom dplyr mutate tibble bind_cols
+#' @importFrom purrr map_chr map_df
+#' @importFrom utils menu
 #' @import rlang
+#' @import glue
 #' @import cli
 
 .env_llm <- new.env()
