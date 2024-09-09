97e75af0de1dc2a0d6d29aa08a350e36eef419d6
Edgar Ruiz <edgararuiz@gmail.com>
2024-09-09 10:40:35 -0500

    Adds help to classify

--- R/llm-classify.R
+++ R/llm-classify.R
@@ -1,6 +1,22 @@
+#' Categorize data as one of options given
+#'
+#' @details
+#' Use a Large Language Model (LLM) to classify the provided text as one of the
+#' options provided via the `labels` argument.
+#' 
+#' @param .data A `data.frame` or `tbl` object that contains the text to be analyzed
+#' @param col The name of the field to analyze, supports `tidy-eval`
+#' @param x A vector that contains the text to be analyzed
+#' @param additional_prompt Inserts this text into the prompt sent to the LLM
+#' @param pred_name A character vector with the name of the new column where the
+#' prediction will be placed. Defaults to `.classify`
+#' @param labels A character vector with at least 2 labels to classify the text
+#' as
+#' @returns `llm_classify` returns a `data.frame` or `tbl` object. `llm_vec_classify`
+#' returns a vector that is the same length as `x`.
 #' @export
 llm_classify <- function(.data,
-                         x = NULL,
+                         col,
                          labels,
                          pred_name = ".classify",
                          additional_prompt = "") {
@@ -9,22 +25,23 @@ llm_classify <- function(.data,
 
 #' @export
 llm_classify.data.frame <- function(.data,
-                                    x = NULL,
+                                    col,
                                     labels,
                                     pred_name = ".classify",
                                     additional_prompt = "") {
   mutate(
     .data = .data,
     !!pred_name := llm_vec_classify(
-      x = {{ x }},
+      x = {{ col }},
       labels = labels,
       additional_prompt = additional_prompt
     )
   )
 }
 
+#' @rdname llm_classify
 #' @export
-llm_vec_classify <- function(x = NULL,
+llm_vec_classify <- function(x,
                              labels,
                              additional_prompt = "") {
   llm_vec_prompt(
