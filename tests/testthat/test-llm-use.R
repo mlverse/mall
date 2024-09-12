c5fa6e834b788a83e03e7ef32282c644e3f2457e
Edgar Ruiz <edgararuiz@gmail.com>
2024-09-12 11:00:41 -0500

    Adds coverage test for llm_use()

---
+++ tests/testthat/test-llm-use.R
@@ -0,0 +1,12 @@
+test_that("Init code is covered", {
+  local_mocked_bindings(
+    test_connection = function() {
+      x <- list()
+      x$status_code <- 200
+      x
+    },
+    list_models = function() data.frame(name = c("model1", "model2")),
+    menu = function(...) 1
+  )
+  expect_message(llm_use(force = TRUE))
+})
