d2466165c97a21ced3e213239eca1ab5689a1eb9
Edgar Ruiz <edgararuiz@gmail.com>
2024-09-12 10:15:22 -0500

    Adds summarize tests

---
+++ tests/testthat/test-llm-summarize.R
@@ -0,0 +1,22 @@
+test_that("Summarize works", {
+  test_text <- "this is a test"
+  llm_use("simulate_llm", "echo", .silent = TRUE)
+  expect_equal(
+    llm_vec_summarize(test_text),
+    test_text
+  )
+
+  expect_equal(
+    llm_summarize(data.frame(x = test_text), x),
+    data.frame(x = test_text, .summary = test_text)
+  )
+
+  expect_equal(
+    llm_summarize(
+      data.frame(x = test_text),
+      x,
+      pred_name = "new"
+    ),
+    data.frame(x = test_text, new = test_text)
+  )
+})
