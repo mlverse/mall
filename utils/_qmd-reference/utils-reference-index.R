7597223d2a80c241f147b16bc538483d169cd925
Edgar Ruiz <edgararuiz@gmail.com>
2024-09-21 17:29:26 -0500

    Further simplifies reference index script

--- utils/_qmd-reference/utils-reference-index.R
+++ utils/_qmd-reference/utils-reference-index.R
@@ -1,17 +1,18 @@
-reference_index <- function(pkg = NULL, folder = "/reference", output = "qmd") {
+reference_index <- function(pkg = NULL, folder = "/reference") {
   if (is.character(pkg)) pkg <- pkgdown::as_pkgdown(pkg)
   ref_list <- reference_to_list_index(pkg)
   dir_out <- path(folder)
   ref_convert <- reference_index_convert(ref_list, dir_out)
   res <- imap(
-    ref_convert, 
+    ref_convert,
     \(.x, .y) {
-    if (.y == 1) {
-      .x
-    } else {
-      c(" ", paste("##", .y), " ", .x)
+      if (.y == 1) {
+        .x
+      } else {
+        c(" ", paste("##", .y), " ", .x)
+      }
     }
-  })
+  )
   res <- reduce(res, c)
   c(
     "---",
@@ -23,40 +24,47 @@ reference_index <- function(pkg = NULL, folder = "/reference", output = "qmd") {
 }
 
 reference_index_convert <- function(index_list, dir_out = "") {
-  out <- map(index_list, ~ map(.x, ~ {
-    # Manual fixes of special characters in funs variable
-    funcs <- .x$funs
-    if (length(funcs) == 0) funcs <- .x$alias
-    funcs <- gsub("&lt;", "<", funcs)
-    funcs <- gsub("&gt;", ">", funcs)
-    funcs <- paste0(funcs, collapse = " \\| ")
-    file_out <- path(dir_out, .x$file_out)
-    desc <- .x$title
-    c(
-      paste0("[", funcs, "](", file_out, ")"),
-      desc
-    )
-  }))
+  out <- map(index_list, \(.x) map(.x, reference_links, dir_out))
   header <- c("Function(s) | Description", "|---|---|")
-  map(out, ~ c(header, map_chr(.x, ~ paste0("|", .x[[1]], "|", .x[[2]], "|"))))
+  map(
+    out,
+    \(.x) {
+      c(
+        header,
+        map_chr(.x, \(.x) paste0("|", .x[[1]], "|", .x[[2]], "|"))
+      )
+    }
+  )
+}
+
+reference_links <- function(x, dir_out) {
+  # Manual fixes of special characters in funs variable
+  funcs <- x$funs
+  if (length(funcs) == 0) funcs <- x$alias
+  funcs <- gsub("&lt;", "<", funcs)
+  funcs <- gsub("&gt;", ">", funcs)
+  funcs <- paste0(funcs, collapse = " \\| ")
+  file_out <- path(dir_out, x$file_out)
+  desc <- x$title
+  c(
+    paste0("[", funcs, "](", file_out, ")"),
+    desc
+  )
 }
 
 reference_to_list_index <- function(pkg) {
   if (is.character(pkg)) pkg <- as_pkgdown(pkg)
   pkg_ref <- pkg$meta$reference
-
   pkg_topics <- pkg$topics
   topics_env <- pkgdown:::match_env(pkg_topics)
-
   if (is.null(pkg_ref)) {
     x <- list(data.frame(contents = pkg_topics$name))
   } else {
     x <- pkg_ref
   }
-
   sections_list <- map(
     seq_along(x),
-    ~ {
+    \(.x) {
       ref <- x[[.x]]
       topic_list <- map(
         ref$contents,
@@ -76,11 +84,9 @@ reference_to_list_index <- function(pkg) {
       transpose(pkg_topics[topic_ids, ])
     }
   )
-
   if (!is.null(pkg_ref)) {
-    sections_title <- map_chr(pkg_ref, ~ .x$title)
+    sections_title <- map_chr(pkg_ref, \(.x) .x$title)
     names(sections_list) <- sections_title
   }
-
   sections_list
 }
