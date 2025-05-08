65a8a67a5965fc22db3ef503b935f7f5185b95e4
Edgar Ruiz <77294576+edgararuiz@users.noreply.github.com>
2025-05-08 09:51:08 -0500

    Adds test for custom

---
+++ python/tests/test_custom.py
@@ -0,0 +1,12 @@
+import pytest
+import mall
+import polars as pl
+import shutil
+import os
+
+def test_custom_prompt():
+    df = pl.DataFrame(dict(x="x"))
+    df.llm.use("test", "content", _cache="_test_cache")
+    x = df.llm.custom("x", "hello")
+    assert (x["custom"][0] == "hello: \n{}")
+    shutil.rmtree("_test_cache", ignore_errors=True)
